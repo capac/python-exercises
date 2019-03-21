0e28eaf7520593b97b5b458ea22f4cab9fe18635
capac <angelovarlotta@gmail.com>
2019-03-21 19:20:21 +0000

    Bug fixes

---
+++ various/employee.py
@@ -0,0 +1,42 @@
+import datetime
+
+
+class Employee:
+    '''Template for company employee'''
+
+    raise_amt = 1.04
+    num_of_emps = 0
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = self.first + '.' + self.last + '@company.com'
+        self.pay = pay
+
+        Employee.num_of_emps += 1
+
+    def __repr__(self):
+        return f'Employee({self.first}, {self.last}, {self.pay})'
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    def apply_raise(self, amount):
+        self.pay = int(self.pay * self.amount)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @staticmethod
+    def is_workday(date):
+        if date.weekday == 5 or date.weekday == 6:
+            return False
+        return True
+
+
+if __name__ == '__main__':
+    emp_1 = Employee('Sue', 'Mitchell', 70000)
+    print(emp_1)
+    date = datetime.date(2019, 3, 14)
+    print(Employee.is_workday(date))
--- various/iter.py
+++ various/iter.py
@@ -1,7 +1,7 @@
 class Range:
     # home-built Range function
     def __init__(self, start, end):
-        #init our Range function it's a special method
+        # init our Range function it's a special method
         self.start = start
         self.end = end
         self.current = start
@@ -14,11 +14,12 @@ class Range:
         # this return the next item in the iteration
         if self.current >= self.start and self.current < self.end:
             val = self.current
-            self.current +=2
+            self.current += 2
             return val
         else:
             raise StopIteration
 
-    # this is for the loop
+
+# this is for the loop
 for i in Range(0, 20):
     print(i)
