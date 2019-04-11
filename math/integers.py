2a014728659cf3b4890a4815e453d08d75746a54
capac <angelovarlotta@gmail.com>
2019-04-11 14:35:11 +0100

    Updated 'integers' file

--- math/integers.py
+++ math/integers.py
@@ -2,11 +2,11 @@ m = 0
 finished = False
 
 while not finished:
-    print("Enter another integer number (0 to finish): ", end="")
-    num = int(input())
+    num = int(input("Enter another integer number (0 to finish): "))
     if num != 0:
         if num > m:
             m = num
     else:
         finished = True
+
 print(str(m))
