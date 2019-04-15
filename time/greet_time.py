122d2de7c410922158e2e95df2d20fe16362f5e9
capac <angelovarlotta@gmail.com>
2019-04-15 17:01:43 +0100

    Added 'Greet' class

--- time/greet_time.py
+++ time/greet_time.py
@@ -1,39 +1,43 @@
-def time_f(time):
-    hours, minutes = time[:-2], time[-2:]
-    if divmod(int(hours), 12)[0] >= 2:
-        print("Hours must be between 0 and 23!")
-        return False
-    if divmod(int(minutes), 60)[0] >= 1:
-        print("Minutes must be between 0 and 59!")
-        return False
-    return True
 
+class Greet():
+    def __init__(self, time):
+        self.time = time
 
-def am_pm(time):
-    hours, minutes = time[:-2], time[-2:]
-    if divmod(int(hours), 12)[0] < 1:
-        return f'{hours}:{minutes} am'
-    elif divmod(int(hours), 12)[0] >= 1 and divmod(int(hours), 12)[0] < 2:
-        return f'{str(int(hours) - 12)}:{minutes} pm'
+    def time_check(self):
+        hours, minutes = self.time[:-2], self.time[-2:]
+        if divmod(int(hours), 12)[0] >= 2:
+            print("Hours must be between 0 and 23!")
+            return False
+        if divmod(int(minutes), 60)[0] >= 1:
+            print("Minutes must be between 0 and 59!")
+            return False
+        return True
+
+    def time_format(self):
+        hours, minutes = self.time[:-2], self.time[-2:]
+        am_pm = 'am' if int(hours) < 12 else 'pm'
+        hours = int(hours) % 12 if int(hours) % 12 != 0 else 12
+        return f'{str(hours)}:{minutes} {am_pm}'
 
 
 if __name__ == "__main__":
     time = input("Enter a number between 0 and 2359 to represent the time: ")
-    if time_f(time):
+    greetings = Greet(time)
+    if greetings.time_check():
         hours = int(time[:-2])
-        if hours > 0 and hours <= 4:
-            print(f"It's {am_pm(time)}, you should be sleeping!")
+        if hours >= 0 and hours < 4:
+            print(f"It's {greetings.time_format()}, you should be sleeping!")
 
-        elif hours > 4 and hours < 12:
-            print(f"It's {am_pm(time)}, good morning!")
+        elif hours >= 4 and hours < 12:
+            print(f"It's {greetings.time_format()}, good morning!")
 
-        elif hours > 12 and hours <= 17:
-            print(f"It's {am_pm(time)}, good afternoon!")
+        elif hours >= 12 and hours < 17:
+            print(f"It's {greetings.time_format()}, good afternoon!")
 
-        elif hours > 17 and hours <= 22:
-            print(f"It's {am_pm(time)}, good evening!")
+        elif hours >= 17 and hours < 22:
+            print(f"It's {greetings.time_format()}, good evening!")
 
-        elif hours > 22 and hours < 24:
-            print(f"It's {am_pm(time)}, good night!")
+        elif hours >= 22 and hours < 24:
+            print(f"It's {greetings.time_format()}, good night!")
         else:
-            print(f"{am_pm(time)}")
+            print(f"{greetings.time_format()}")
