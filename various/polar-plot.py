15537376bf1f01b3dc31d1144badacead86e9abc
capac <capac@users.noreply.github.com>
2019-11-03 14:54:43 +0000

    Decreased axis line thickness

--- various/polar-plot.py
+++ various/polar-plot.py
@@ -16,10 +16,11 @@ colors = theta
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='polar')
 c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.6, edgecolor='k')
-c.set_linewidth(2)
+lw=1.5
+c.set_linewidth(lw)
 plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
-plt.setp(ax.spines.values(), linewidth=2)
-
+plt.setp(ax.spines.values(), linewidth=lw)
+plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
 # fontsize=8
 # for tick in ax.xaxis.get_major_ticks():
 #     tick.label1.set_fontsize(fontsize)
@@ -27,6 +28,6 @@ plt.setp(ax.spines.values(), linewidth=2)
 # for tick in ax.yaxis.get_major_ticks():
 #     tick.label1.set_fontsize(fontsize)
 #     tick.label1.set_fontweight('bold')
-plt.grid(linewidth=2, alpha=0.4, color='k')
+plt.grid(linewidth=lw, alpha=0.4, color='k')
 plt.show()
 # plt.savefig('polar-plot.png', dpi=144, format='png')
