24b8a2302520c7b2a4c2daf24a33676a37796dcb
capac <capac@users.noreply.github.com>
2019-11-03 13:46:03 +0000

    Removed labels

--- various/polar-plot.py
+++ various/polar-plot.py
@@ -7,24 +7,26 @@ import matplotlib.pyplot as plt
 np.random.seed(196)
 
 # Compute areas and colors
-N = 150
+N = 80
 r = 2 * np.random.rand(N)
 theta = 2 * np.pi * np.random.rand(N)
-area = 300 * r**2
+area = 600 * r**2
 colors = theta
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='polar')
-c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75, edgecolor='k')
-c.set_linewidth(0.5)
-plt.setp(ax.spines.values(), linewidth=1.5)
-fontsize=8
-for tick in ax.xaxis.get_major_ticks():
-    tick.label1.set_fontsize(fontsize)
-    tick.label1.set_fontweight('bold')
-for tick in ax.yaxis.get_major_ticks():
-    tick.label1.set_fontsize(fontsize)
-    tick.label1.set_fontweight('bold')
-plt.grid(linewidth=1.5, alpha=0.4, color='k')
+c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.6, edgecolor='k')
+c.set_linewidth(2)
+plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
+plt.setp(ax.spines.values(), linewidth=2)
+
+# fontsize=8
+# for tick in ax.xaxis.get_major_ticks():
+#     tick.label1.set_fontsize(fontsize)
+#     tick.label1.set_fontweight('bold')
+# for tick in ax.yaxis.get_major_ticks():
+#     tick.label1.set_fontsize(fontsize)
+#     tick.label1.set_fontweight('bold')
+plt.grid(linewidth=2, alpha=0.4, color='k')
 plt.show()
 # plt.savefig('polar-plot.png', dpi=144, format='png')
