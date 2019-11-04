f263df6d38a54a864fbb4f00e07a108b65eac137
capac <capac@users.noreply.github.com>
2019-11-04 08:51:36 +0000

    Changed border thicknesses

--- various/polar-plot.py
+++ various/polar-plot.py
@@ -16,8 +16,8 @@ colors = theta
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='polar')
 c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.6, edgecolor='k')
-lw=1.5
-c.set_linewidth(lw)
+lw, bw = 2, 1
+c.set_linewidth(bw)
 # plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
 plt.setp(ax.spines.values(), linewidth=lw)
 # plt.subplots_adjust(left=0.05, right=0.95, top=0.98, bottom=0.05)
