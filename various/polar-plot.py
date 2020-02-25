1bbb113b4f50cb8c1c8f9204682dfbeebea3400c
capac <capac@users.noreply.github.com>
2020-02-25 14:21:57 +0000

    Removed labels

--- various/polar-plot.py
+++ various/polar-plot.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 np.random.seed(196)
 
 # Compute areas and colors
-N = 80
+N = 45
 r = 2 * np.random.rand(N)
 theta = 2 * np.pi * np.random.rand(N)
-area = 600 * r**2
+area = 1600 * r**2
 colors = theta
 
 fig = plt.figure()
@@ -18,7 +18,7 @@ ax = fig.add_subplot(111, projection='polar')
 c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.6, edgecolor='k')
 lw, bw = 2, 1
 c.set_linewidth(bw)
-# plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
+plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
 plt.setp(ax.spines.values(), linewidth=lw)
 # plt.subplots_adjust(left=0.05, right=0.95, top=0.98, bottom=0.05)
 fontsize=8
