3b6abab9788e00ca5a5fcc6a90ae79c74923d6f6
capac <capac@users.noreply.github.com>
2021-02-19 17:04:18 +0000

    New exercise

---
+++ hackerrank/grading_students.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    new_grades = []
+    for grade in grades:
+        if grade < 38:
+            new_grades.append(grade)
+            continue
+        if 5 - (grade % 5) < 3:
+            new_grade = grade + 5 - (grade % 5)
+            new_grades.append(new_grade)
+        elif 5 - (grade % 5) >= 3:
+            new_grades.append(grade)
+    return new_grades
+
+
+if __name__ == '__main__':
+    arr = input('Provide an array of grades: ')
+    result = gradingStudents(arr)
+    print(result)
