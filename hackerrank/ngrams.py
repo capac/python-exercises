0cb3a4f330a395b5a8847ab24926912a04d903a0
capac <capac@users.noreply.github.com>
2021-02-23 16:06:22 +0000

    Removed whitespace

--- hackerrank/ngrams.py
+++ hackerrank/ngrams.py
@@ -2,13 +2,13 @@
 
 
 def create_ngrams(string, num):
-    arr = list(string) 
+    arr = list(string)
     num = int(num)
     return list(zip(*[arr[i:] for i in range(num)]))
 
 
 if __name__ == '__main__':
-    s = input('Please provide string: ')
-    n = input('Please provide n-gram length: ')
-    res = create_ngrams(s, n)
+    string = input('Please provide string: ')
+    num = input('Please provide n-gram length: ')
+    res = create_ngrams(string, num)
     print(res)
