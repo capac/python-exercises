553b8d080ffb729ca76732af36e43fcf3d41d2c4
capac <capac@users.noreply.github.com>
2021-06-10 16:57:47 +0100

    Normal CDF function and plots

---
+++ data-science-from-scratch/cdf_functions.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (1 + math.erf((x - mu) / (math.sqrt(2) * sigma))) / 2
+
+
+def normal_cdf_plots(xs, mu, sigma, color):
+    axes.plot(xs, [normal_cdf(x, mu, sigma) for x in xs], color=color,
+              label=f'mu = {mu}, sigma = {sigma}')
+    axes.set_title('Various normal CDFs')
+    axes.legend(loc=4)
+    fig.tight_layout()
+    plt.grid(True, linestyle='--')
+
+
+# parameter values
+mu_list = [0, 0, 0, -1]
+sigma_list = [1, 2, 0.5, 1]
+colors = ['dodgerblue', 'forestgreen', 'firebrick', 'darkorchid']
+
+# plots
+xs = np.linspace(-6, 6, 100)
+fig, axes = plt.subplots(1, 1, figsize=(8, 6))
+for mu, sg, cl in zip(mu_list, sigma_list, colors):
+    normal_cdf_plots(xs, mu, sg, cl)
+plt.show()
