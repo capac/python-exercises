0fa58401d160a702a8cfba3f406275f857e7a203
capac <capac@users.noreply.github.com>
2021-06-18 12:58:43 +0100

    Modified to PEP 8 standards

--- various/polar-plot.py
+++ various/polar-plot.py
@@ -18,10 +18,11 @@ ax = fig.add_subplot(111, projection='polar')
 c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.6, edgecolor='k')
 lw, bw = 2, 1
 c.set_linewidth(bw)
-plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
+plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False,
+                right=False, left=False, labelleft=False)
 plt.setp(ax.spines.values(), linewidth=lw)
 # plt.subplots_adjust(left=0.05, right=0.95, top=0.98, bottom=0.05)
-fontsize=8
+fontsize = 8
 for tick in ax.xaxis.get_major_ticks():
     tick.label1.set_fontsize(fontsize)
     tick.label1.set_fontweight('bold')
